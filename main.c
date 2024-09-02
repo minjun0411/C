4efb9a6daf0ee5a7c917fbb73f93ef977a2a7031
minjun0411 <minjun9977@gmail.com>
2024-09-02 19:39:22 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int number[10];
+    
+    srand(time(NULL));
+    
+    for(int i = 0; i<10; i++){
+        number[i] = (rand() % 100) + i;
+    }
+    
+    for(int i = 0; i<10; i++){
+        printf("number = %d\n", number[i]);
+    }
+    return 0;
+}
